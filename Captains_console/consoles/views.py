f70a5d6e2d751ec614b4d79ac3b2a90bb5aeb8b0
SILLI20 <sigvaldi19@ru.is>
2020-05-04 20:10:04 +0000

    fixing packes

--- Captains_console/consoles/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-from consoles.models import Consoles
-# Create your views here.
-
-def index(request):
-    context = {'consoles': Consoles.objects.all().order_by('name')}
-    return render(request, 'consoles/index.html', context)
