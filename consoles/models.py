b7357aea9585bea74ce299376869d760209c9213
SILLI20 <sigvaldi19@ru.is>
2020-05-05 14:24:37 +0000

    added to database and added more things to console models

--- consoles/models.py
+++ consoles/models.py
@@ -4,11 +4,11 @@ from django.db import models
 # Create your models here.
 
 class Consoles(models.Model):
-    name = models.CharField(max_length=255)
-    # description = models.CharField(max_length=999, blank=True)
-    # logo = models.CharField(max_length=999, blank=True)
-    # year_created = models.DateTimeField(null=True)
-    price = models.FloatField()
+    name = models.CharField(max_length=999, blank=True)
+    image = models.CharField(max_length=999, blank=True)
+    release_date = models.DateField(blank=True)
+    description = models.CharField(max_length=99999, blank=True)
+    price = models.FloatField(blank=True)
 
     def __str__(self):
         return self.name
--- games/models.py
+++ games/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-# from consoles.models import Consoles
+from consoles.models import Consoles
 
 
 # Create your models here.
 
 class Games(models.Model):
-    name = models.CharField(max_length=255)
-    # description = models.CharField(max_length=999, blank=True)
-    # console = models.ForeignKey(Consoles, on_delete=models.CASCADE)
-    price = models.FloatField()
+    name = models.CharField(max_length=255, null=True)
+    description = models.CharField(max_length=999, blank=True)
+    console = models.ForeignKey(Consoles, on_delete=models.CASCADE, null=True)
+    price = models.FloatField(null=True)
 
     def __str__(self):
         return self.name
