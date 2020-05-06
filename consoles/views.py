bed6a7b5d4e0a9e403dc91d6e32c9fcb6331b7f4
SILLI20 <sigvaldi19@ru.is>
2020-05-06 12:31:18 +0000

    games and consoles now clickable

--- consoles/views.py
+++ consoles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from consoles.models import Consoles
 # Create your views here.
@@ -6,3 +6,8 @@ from consoles.models import Consoles
 def index(request):
     context = {'consoles': Consoles.objects.all().order_by('name')}
     return render(request, 'consoles/index.html', context)
+
+def get_console_by_id(request, id):
+    return render(request, 'consoles/console_details.html', {
+        'console': get_object_or_404(Consoles, pk=id)
+    })
