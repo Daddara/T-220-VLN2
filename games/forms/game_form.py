25e176308692901269947d61b1977da2930b95ab
SILLI20 <sigvaldi19@ru.is>
2020-05-07 14:16:55 +0000

    adding delete and update buttons

--- games/forms/game_form.py
+++ games/forms/game_form.py
@@ -1,15 +1,30 @@
 from django.forms import ModelForm, widgets
 from games.models import Games
 
-class GameCreateForm(ModelForm):
+
+class GameUpdateForm(ModelForm):
     class Meta:
         model = Games
-        exclude = [ 'id' ]
+        exclude = ['id']
         widgets = {
-            'name': widgets.TextInput(attrs={ 'class': 'form-control'}),
+            'name': widgets.TextInput(attrs={'class': 'form-control'}),
             'description': widgets.TextInput(attrs={'class': 'form-control'}),
             'price': widgets.NumberInput(attrs={'class': 'form-control'}),
             'console': widgets.TextInput(attrs={'class': 'form-control'}),
             'image': widgets.TextInput(attrs={'class': 'form-control'}),
-            'release_date':  widgets.TextInput(attrs={'class': 'form-control'})
+            'release_date': widgets.TextInput(attrs={'class': 'form-control'})
+        }
+
+
+class GameCreateForm(ModelForm):
+    class Meta:
+        model = Games
+        exclude = ['id']
+        widgets = {
+            'name': widgets.TextInput(attrs={'class': 'form-control'}),
+            'description': widgets.TextInput(attrs={'class': 'form-control'}),
+            'price': widgets.NumberInput(attrs={'class': 'form-control'}),
+            'console': widgets.TextInput(attrs={'class': 'form-control'}),
+            'image': widgets.TextInput(attrs={'class': 'form-control'}),
+            'release_date': widgets.TextInput(attrs={'class': 'form-control'})
         }
--- games/views.py
+++ games/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404, redirect
 
-from games.forms.game_form import GameCreateForm
+from games.forms.game_form import GameCreateForm, GameUpdateForm
 from games.models import Games
 
 
 # Create your views here.
 
 def index(request):
+    if 'search_filter' in request.GET:
+        search_filter = request.GET['search_filter']
+        games = list(Games.objects.filter(name__icontains=search_filter).values())
+        return JsonResponse({'data': games})
     context = {'games': Games.objects.all().order_by('name')}
     return render(request, 'games/index.html', context)
 
@@ -16,11 +21,38 @@ def get_game_by_id(request, id):
         'game': get_object_or_404(Games, pk=id)
     })
 
+
 def create_game(request):
     if request.method == "POST":
-        print(1)
+        form = GameCreateForm(data=request.POST)
+        if form.is_valid():
+            game = form.save()
+            return redirect('games-index')
     else:
         form = GameCreateForm()
     return render(request, 'games/create_game.html', {
         'form': form
     })
+
+
+def delete_game(request, id):
+    game = get_object_or_404(Games, pk=id)
+    game.delete()
+    return redirect('games-index')
+
+
+def update_game(request, id):
+    instance = get_object_or_404(Games, pk=id)
+    if request.method == "POST":
+        form = GameUpdateForm(data=request.POST, instance=instance)
+        if form.is_valid():
+            form.save()
+            return redirect('games_details', id=id)
+    else:
+        form = GameUpdateForm(instance=instance)
+    return render(request, 'games/update_game.html', {
+        'form': form,
+        'id': id
+    })
+
+
