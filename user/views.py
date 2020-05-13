c56536cd8c73ae70d4b693810d7176a03f6b3795
Kristjamar <kristjan1405@gmail.com>
2020-05-13 16:32:16 +0000

    fokk já

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from user.forms.profile_form import ProfileForm
 from user.models import Profile
 
 
@@ -16,7 +17,12 @@ def register(request):
 def profile(request):
     profile = Profile.objects.filter(user=request.user).first()
     if request.method == 'POST':
-        print(1)
+        form = ProfileForm(instance=profile, data=request.POST)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('profile')
     return render(request, 'user/profile.html', {
-        'form': ''
+        'form': ProfileForm(instance=profile)
     })
