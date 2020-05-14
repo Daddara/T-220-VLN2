68787430a766e89cfc32008d98da1c521ce0032e
SILLI20 <sigvaldi19@ru.is>
2020-05-14 20:24:57 +0000

    price in checkout

--- checkout/views.py
+++ checkout/views.py
@@ -9,13 +9,16 @@ from games.models import Games
 def index(request):
     cart_instances = Cart.objects.filter(u=request.user)
     product_list = []
+    cart_ids = []
+    price = 0
     for instance in cart_instances:
         if instance.game != None:
             game = Games.objects.filter(pk=instance.game.id).first()
             product_list.append(game)
+            price += game.price
         else:
             console = Consoles.objects.filter(pk=instance.console.id).first()
             product_list.append(console)
-
-    context = {'cart': product_list}
+            price += console.price
+    context = {'cart': product_list, 'total_price': price}
     return render(request, 'checkout/checkout.html', context)
