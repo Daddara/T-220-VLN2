2b9c63db370cac18b5c2fa95ef0c13323573c806
SILLI20 <sigvaldi19@ru.is>
2020-05-14 17:23:44 +0000

    pushing

--- checkout/views.py
+++ checkout/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 from cart.models import Cart
+from consoles.models import Consoles
+from games.models import Games
 
 
 def index(request):
-    context = {'cart': Cart.objects.all().order_by('game')}
+    cart_instances = Cart.objects.filter(u=request.user)
+    product_list = []
+    for instance in cart_instances:
+        if instance.game != None:
+            game = Games.objects.filter(pk=instance.game.id).first()
+            product_list.append(game)
+        else:
+            console = Consoles.objects.filter(pk=instance.console.id).first()
+            product_list.append(console)
+
+    context = {'cart': product_list}
     return render(request, 'checkout/checkout.html', context)
