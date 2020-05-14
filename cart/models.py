76ccdfda1ac7c9f2718a9172f78cd5ba77c415cc
SILLI20 <sigvaldi19@ru.is>
2020-05-14 17:34:09 +0000

    migration

--- cart/models.py
+++ cart/models.py
@@ -10,5 +10,5 @@ class Cart(models.Model):
     u = models.ForeignKey(User,  on_delete=models.CASCADE)
     game = models.ForeignKey(Games, on_delete=models.CASCADE, blank=True, null=True)
     console = models.ForeignKey(Consoles, on_delete=models.CASCADE, blank=True, null=True)
-    quantity = models.DecimalField(max_digits=3, decimal_places=2)  # 1
+    quantity = models.DecimalField(max_digits=5, decimal_places=2)  # 1
 
