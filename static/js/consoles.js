45589c2532424a5fa087ebd00d5c1b37bd701762
Arnar <arnarrvk@gmail.com>
2020-05-14 00:08:44 +0000

    added filter option -no function yet. Improved sorting window

--- static/js/consoles.js
+++ static/js/consoles.js
@@ -34,3 +34,11 @@ $(document).ready(function () {
     });
 
 });
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".dropdown-menu li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
