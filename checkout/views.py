e1129219286db7253895b1074952f32894252f93
SILLI20 <sigvaldi19@ru.is>
2020-05-14 21:48:43 +0000

    pulling

--- checkout/views.py
+++ checkout/views.py
@@ -15,10 +15,12 @@ def index(request):
         if instance.game != None:
             game = Games.objects.filter(pk=instance.game.id).first()
             product_list.append(game)
+            cart_ids.append(instance.id)
             price += game.price
         else:
             console = Consoles.objects.filter(pk=instance.console.id).first()
             product_list.append(console)
+            cart_ids.append(instance.id)
             price += console.price
-    context = {'cart': product_list, 'total_price': price}
+    context = {'cart': product_list, 'total_price': price, 'cart_id': cart_ids}
     return render(request, 'checkout/checkout.html', context)
